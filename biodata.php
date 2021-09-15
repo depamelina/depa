a4416c353d85bac0440db99121575af1c24fe56d
Depa Melina <pchdaniel10@gmail.com>
2021-09-15 14:22:14 +0700

    Create biodata.php

---
+++ biodata.php
@@ -0,0 +1,26 @@
+<?php 
+$nama = "Depa Melina";
+$prodi = "MI";
+$univ = "LP3I";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata</title>
+</head>
+<body>
+
+		<?php 
+			echo "Biodata";
+			echo "<br>"; 
+			echo "<hr>"; 
+			echo "Nama : ".$nama;
+			echo "<br>"; 
+			echo "Prodi : ".$prodi;
+			echo "<br>"; 
+			echo "Universitas : ".$univ;
+		?>
+
+</body>
+</html>
