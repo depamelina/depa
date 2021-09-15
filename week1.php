38b6b01e2dc71a5a97106eae455af4da7b8cc9bb
Depa Melina <pchdaniel10@gmail.com>
2021-09-15 14:21:59 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,21 @@
+<?php 
+$nama = "Depa";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Week 1 - Depa Melina</title>
+</head>
+<body>
+		<?php
+			echo "Hello World";
+			echo "<br>"; 
+			echo $nama;
+			echo "<br>"; 
+			echo "Hello ".$nama;
+		?>
+</body>
+</html>
+
+
+
