2d10affb916cd5014e4973fd42d7fc892f0e4804
Depa Melina <pchdaniel10@gmail.com>
2021-09-15 14:22:08 +0700

    Create array.php

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php 
+$biodata = array(
+	'nama' => "Depa",
+	'kelas' => "MI20B");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Array</title>
+</head>
+<body>
+	<?php 
+			echo $biodata['nama'];
+
+	?>
+</body>
+</html>
