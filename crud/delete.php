18dab5617b9551a09f00b55688bc39ecca999428
Depa Melina <pchdaniel10@gmail.com>
2021-10-27 10:02:29 +0700

    Create delete.php

---
+++ crud/delete.php
@@ -0,0 +1,6 @@
+<?php
+$con = new mysqli("localhost","root","","db_surat");
+$id = $_GET['id'];
+$result = mysqli_query($con, "DELETE FROM `tbl_surat` WHERE `id`='$id'");
+header("location: view.php");
+?>
